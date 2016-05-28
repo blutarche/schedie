d489a7743b65a9c904572c6061ad4178a9b1734c
Kanitkorn S <k.sujautra@gmail.com>
2016-05-29 06:27:39 +0700

    Separate title and time object

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ const exec = require('child_process').exec
 const app = express()
 const port = process.env.PORT || 3000
 const corenlp = {
-  'host': 'corenlp.run',
+  'host': 'localhost:9000',
   'properties': {
-    "tokenize.whitespace": "true",
-    "annotators": "ner",
-    "outputFormat": "json"
+    'tokenize.whitespace': 'true',
+    'annotators': 'ner', // tokenize, ssplit, pos, lemma, ner, depparse, coref, natlog, openie
+    'outputFormat': 'json'
   }
 }
 
@@ -23,11 +23,39 @@ app.get('/', function (req, res) {
 })
 
 app.post('/', function (req, res) {
-  exec(`wget --post-data '${req.body.data}' '${corenlp.host}/?properties=${JSON.stringify(corenlp.properties)}' -O -`, function (err, stdout) {
+  const input = req.body.data
+  const command = `wget --post-data '${input}' '${corenlp.host}/?properties=${JSON.stringify(corenlp.properties)}' -O -`
+  exec(command, function (err, stdout) {
     if (err) {
       res.status(300).send('Error')
     } else {
-      res.status(200).send(stdout)
+      const output = JSON.parse(stdout)
+      const tokens = output.sentences[0].tokens.map(function (token) {
+        return {
+          word: token.word,
+          pos: token.pos,
+          ner: token.ner,
+          normalizedNER: token.normalizedNER
+        }
+      })
+      let title = []
+      let time = []
+      tokens.forEach(function (token, index) {
+        if (isNERDateTime(token)) {
+          if (index === 0 || isNERDateTime(tokens[index - 1])) {
+            time.push(token)
+          } else if (tokens[index - 1].pos === 'IN' || tokens[index - 1].pos === ':') {
+            time.push(title.pop(), token)
+          } else {
+            title.push(token)
+          }
+        } else {
+          title.push(token)
+        }
+      })
+      console.log(`Title: ${title}`)
+      console.log(`Time: ${time}`)
+      res.status(200).send(JSON.stringify(tokens))
     }
   })
 })
@@ -35,3 +63,7 @@ app.post('/', function (req, res) {
 app.listen(port, function () {
   console.log(`Click me if your terminal isn't suck http://localhost:${port}`)
 })
+
+function isNERDateTime(token) {
+  return token.ner === 'DATE' || token.ner === 'TIME'
+}
